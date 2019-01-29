46e315d65c17a1c097968430551828bcf360aa72
ilya <shishlachevilya@gmail.com>
2019-01-29 14:30:36 +0200

    text indent webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,68 +3,71 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/index.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist'
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: '/node_modules/'
-    }, {
-      test: /\.sass$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: {sourceMap: true}
-        }, {
-          loader: 'postcss-loader',
-          options: {sourceMap: true, config: {path: 'src/js/postcss.config.js'}}
-        }, {
-          loader: 'sass-loader',
-          options: {sourceMap: true}
-        }
-      ]
-    }, {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: {sourceMap: true}
-        }, {
-          loader: 'postcss-loader',
-          options: {sourceMap: true, config: {path: 'src/js/postcss.config.js'}}
-        }
-      ]
-    }, {
-      test: /\.(png|jpg|gif|svg)$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {},
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/'
         },
-      ],
-    },]
-  },
-  devServer: {
-    overlay: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: "index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css"
-    })
-  ],
+        {
+            test: /\.sass$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {sourceMap: true}
+                }, {
+                    loader: 'postcss-loader',
+                    options: {sourceMap: true, config: {path: 'src/js/postcss.config.js'}}
+                }, {
+                    loader: 'sass-loader',
+                    options: {sourceMap: true}
+                }
+            ]
+        },
+        {
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {sourceMap: true}
+                }, {
+                    loader: 'postcss-loader',
+                    options: {sourceMap: true, config: {path: 'src/js/postcss.config.js'}}
+                }
+            ]
+        },
+        {
+            test: /\.(png|jpg|gif|svg)$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {},
+                },
+            ],
+        },]
+    },
+    devServer: {
+        overlay: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: "index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })
+    ],
 };
